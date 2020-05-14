5eec6c7700227025f1aaeb108cee6a1b3560319e
unknown <miteshbhanushali08+1@gmail.com>
2020-05-14 11:14:17 +0530

    Updates

---
+++ precache-manifest.068d81695ee83328ba31163679b8add3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1bf67463cbaa6e604e57c0fba918f4",
+    "url": "/e-store/index.html"
+  },
+  {
+    "revision": "3a2e1e869fcdf97cf9bf",
+    "url": "/e-store/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e581ad8afe469da73888",
+    "url": "/e-store/static/js/2.729ca4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-store/static/js/2.729ca4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2e1e869fcdf97cf9bf",
+    "url": "/e-store/static/js/main.bcee7c51.chunk.js"
+  },
+  {
+    "revision": "c4c33b00a1ed8703ea59",
+    "url": "/e-store/static/js/runtime-main.7e322940.js"
+  }
+]);
